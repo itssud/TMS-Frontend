0f5a3e0625c9e0059ec6412099e719c26aacfd91
Sudarshan Shenbagaraj <sudarshan.shenbagaraj@yokogawa.com>
2022-10-29 12:46:46 +0530

    tea

--- src/app/emp-applied/emp-applied.component.ts
+++ src/app/emp-applied/emp-applied.component.ts
@@ -10,22 +10,15 @@ import { HrdbservicesService } from '../hrdbservices.service';
 })
 export class EmpAppliedComponent implements OnInit {
   trainings: any;
-  appliedTrainings: any[] = [];
+  appliedTrainings: any;
   constructor(public ob: EmployeeservicesService, ob1: HrdbservicesService) {
     ob1.GetAllTraining().subscribe((r) => {
       ob.GetAppliedTrainings().subscribe((res) => {
-        this.appliedTrainings = res;
-        this.trainings = r.filter((b) =>
-          res.map((a) => a.trainingId).includes(b.id)
-        );
-
-        this.trainings = this.trainings.map((train: any) => {
-          res.forEach((at) => {
-            if (at.trainingId === train.id) {
-              return { ...train, status: at.status, message: at.message };
-            }
-          });
-        });
+        // this.appliedTrainings = res.map((a) => a.trainingId);
+        // this.trainings = r.filter((b) =>
+        //   res.map((a) => a.trainingId).includes(b.id)
+        // );
+        this.trainings = res;
       });
     });
   }
